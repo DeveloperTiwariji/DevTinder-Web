df43972fbdebc12158d654223a7cc63f611101e3
Satyam Tiwari <tiwarisatyam4685@gmail.com>
2025-03-31 17:26:44 +0530

    After making BASE_URL dynamic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
-export const BASE_URL = "/api"; // convert loacal to "/api" for production
+// export const BASE_URL = "http://localhost:3000"; // convert loacal to "/api" for production
+
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
